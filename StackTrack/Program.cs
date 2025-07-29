f9c6ddcda33484078095a7864b86a321e94821a7
Marcus <marcuskristensen10@hotmail.com>
2025-07-29 08:44:25 +0200

    Currently changing stuff around.

--- StackTrack/Program.cs
+++ StackTrack/Program.cs
@@ -9,21 +9,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var supabaseUrl = builder.Configuration["Supabase:Url"];
+var supabaseKey = builder.Configuration["Supabase:Key"];
+var options = new SupabaseOptions { AutoConnectRealtime = true };
+
+//var url = Environment.GetEnvironmentVariable("https://xrpfkqjdtwbslcrnztbd.supabase.co"); // Project URL
+//var key = Environment.GetEnvironmentVariable("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycGZrcWpkdHdic2xjcm56dGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MDAxOTgsImV4cCI6MjA2OTI3NjE5OH0.3johaDis8LMEgYwQb_SfaS0VEhSGTeiK41X9Nilqd54"); //Anon Api Key
+
 //Adding Supabase to builder.
-builder.Services.AddScoped(sp =>
-{
-
-    var url = Environment.GetEnvironmentVariable("https://xrpfkqjdtwbslcrnztbd.supabase.co"); // Project URL
-    var key = Environment.GetEnvironmentVariable("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycGZrcWpkdHdic2xjcm56dGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MDAxOTgsImV4cCI6MjA2OTI3NjE5OH0.3johaDis8LMEgYwQb_SfaS0VEhSGTeiK41X9Nilqd54"); //Anon Api Key
-
-    var options = new SupabaseOptions
-    {
-        AutoConnectRealtime = true
-    };
-
-    var client = new Client(url!, key!, options);
-
-    // initialize the client before anyone tries to use it
+builder.Services.AddScoped(sp => {
+    var client = new Client(supabaseUrl, supabaseKey, options);
     client.InitializeAsync().GetAwaiter().GetResult();
     return client;
 });
