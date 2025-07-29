ca27c4861e52a53b83a04ff353ec701edaa1dcde
Marcus <marcuskristensen10@hotmail.com>
2025-07-29 08:11:14 +0200

    Created all properties needed for Loggin user in.

--- StackTrack/Models/LoginModel.cs
+++ StackTrack/Models/LoginModel.cs
@@ -1,6 +1,14 @@
-﻿namespace StackTrack.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StackTrack.Models
 {
     public class LoginModel
     {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+   
     }
 }
